be4f7c1fc3322a1bcd2c1d933733fc849428d38a
w-felipe360 <w.felipebraz@gmail.com>
2024-07-15 19:27:52 -0300

    Update httpStatusCodes with new status codes

--- back-end/src/utils/httpStatusCodes.js
+++ back-end/src/utils/httpStatusCodes.js
@@ -1,11 +1,19 @@
 const OK = 200;
-const NOT_FOUND = 404;
+const CREATED = 201;
 const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const NOT_FOUND = 404;
 const INTERNAL_SERVER_ERROR = 500;
 const CONFLICT = 409;
+const FORBIDDEN = 403;
 
-module.exports = { OK,
+module.exports = {
+  OK,
+  CREATED,
   NOT_FOUND,
+  UNAUTHORIZED,
+  FORBIDDEN,
   BAD_REQUEST,
   INTERNAL_SERVER_ERROR,
-  CONFLICT };
+  CONFLICT,
+};
