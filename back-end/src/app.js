b000d51e51f9a3411dde8b89bdac4e30eb712ba2
w-felipe360 <w.felipebraz@gmail.com>
2024-07-16 01:54:34 -0300

    Update npm dependencies and add CORS package

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { productsRouter, usersRouter } = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
 
